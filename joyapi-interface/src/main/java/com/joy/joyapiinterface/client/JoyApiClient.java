d5b07a48954c80332aa8d35778f4b5582ee5c48a
jason <2650498254@qq.com>
2024-03-07 21:27:58 +0800

    更新样式、微调细节

---
+++ joyapi-interface/src/main/java/com/joy/joyapiinterface/client/JoyApiClient.java
@@ -0,0 +1,10 @@
+/**
+*@program: joyapi-interface
+*@ClassName: JoyApiClient
+*@description: 调用第三方接口的客户端
+*@author: Jason
+*@create: 2024-03-06 21:57
+*@from: <a href="https://www.ojason.top">我的博客</a>
+*/
+public class JoyApiClient {
+}
---
+++ joyapi-interface/src/test/java/com/joy/joyapiinterface/client/JoyApiClientTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+/**
+* @ClassName : JoyApiClientTest  
+* @Description :   
+* @Author : Jason  
+* @Date: 2024-03-06 22:13  
+*/
+class JoyApiClientTest {
+  
+}
