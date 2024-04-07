071490665ddba31a95335da15e7e35d8db483d62
jason <2650498254@qq.com>
2024-04-07 23:21:24 +0800

    update 更新主页样式

--- joyapi-front-reception/src/components/header/TipMenu.vue
+++ joyapi-front-reception/src/components/header/TipMenu.vue
@@ -1,14 +1,43 @@
 <script setup lang="ts" name="tip-menu">
-const tipStr = 'This is a tip menu';
+import { ref } from 'vue';
+
+const tipStr = 'Joy Api - 永不收费';
+const disabled = ref(false);
 </script>
 
 <template>
   <div>
-    {{ tipStr }}
+    <el-tooltip :disabled="disabled" placement="bottom" effect="light">
+      <span class="tip-title">{{ tipStr }}</span>
+      <template #content>
+        <div class="tip-content">
+          <el-button plain>网站公告</el-button>
+          <el-button plain>开源项目</el-button>
+          <el-button plain>友链</el-button>
+        </div>
+      </template>
+    </el-tooltip>
   </div>
 </template>
 
 <style scoped lang="scss">
+.tip-content {
+  width: auto;
+  padding: 10px;
+  background-color: #fff;
+  border-radius: 5px;
+  color: #303133;
+  font-size: 14px;
+  text-align: center;
+  cursor: pointer;
+}
+
+.tip-title {
+  font-size: 20px;
+  color: #409eff;
+  cursor: pointer;
+}
+
 .el-dropdown-link:focus {
   outline: none;
 }
